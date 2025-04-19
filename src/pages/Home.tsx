09cdb716ba3ff11e7ca2dabb3686f61f5ff5f752
unknown <yatog2013@gmail.com>
2025-04-18 23:12:22 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold">Toque na tela para começar</h1>
+    </div>
+  );
+}
